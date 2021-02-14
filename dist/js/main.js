7d741d5e4cf425c7993fb34221b3c6feebfc9bb2
TAsif <ta.asif100@gmail.com>
2021-02-13 21:01:56 -0500

    added scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,11 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.nav-item');
 
+const home = document.querySelector('.home')
+const aboutMe = document.querySelector('.about-me')
+const projects = document.querySelector('.projects')
+const contactMe = document.querySelector('.contact-me')
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -25,4 +30,34 @@ function toggleMenu() {
 
     showMenu = false;
   }
+}
+
+window.smoothScroll = function(target) {
+  var scrollContainer = target;
+  do {
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+  } while (scrollContainer.scrollTop == 0);
+
+  var targetY = 0;
+  do {
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+  } while (target = target.offsetParent);
+
+  scroll = function(c, a, b, i) {
+      i++; if (i > 30) return;
+      c.scrollTop = a + (b - a) / 30 * i;
+      setTimeout(function(){ scroll(c, a, b, i); }, 10);
+  }
+  
+  home.classList.remove('current')
+  aboutMe.classList.remove('current')
+  projects.classList.remove('current')
+  contactMe.classList.remove('current')
+
+  scrollContainer.classList.add('current')
+
+  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
